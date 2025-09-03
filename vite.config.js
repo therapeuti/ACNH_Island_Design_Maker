a15b27034b4ea43566fe5249e416d0bea29f13c0
therapeuti <lorraine_dubled@gmail.com>
2025-09-03 17:19:55 +0900

    feat: netlify로 배포 시도

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/ACNH_Island_Design_Maker/' : './',
+  base: './',
   build: {
     outDir: 'dist'
   }
