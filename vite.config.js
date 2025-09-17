f0b5a1fc16950397c467fba80547fc91fa3597d6
therapeuti <lorraine.dubled@gmail.com>
2025-09-17 11:52:55 +0900

    GitHub Pages 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ACNH_island_design_maker/',
+  base: '/ACNH_Island_Design_Maker/',
   build: {
     outDir: 'dist'
   }
